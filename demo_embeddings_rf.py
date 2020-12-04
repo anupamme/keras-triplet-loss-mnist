55b271167b67e8d71b2fc91f7caaecc4f1a31557
Laptop <laptop@Laptops-MacBook-Pro.local>
2020-12-04 10:38:19 +0000

    adding character embeddings

--- demo_embeddings_rf.py
+++ demo_embeddings_rf.py
@@ -66,41 +66,35 @@ def get_model_LSTM_char(vocab_size, input_size, num_of_classes=2, add_multiple_i
                             weights=[embedding_weights])
     inputs = tf.keras.layers.Input(shape=(input_size,), name='input', dtype='int64')
     
-    # Embedding
-    input_1, input_2 = tf.split(inputs, 2)
-    
-    x1 = embedding_layer(input_1)
-    x1 = tf.keras.layers.Flatten()(x1)
-    
     if add_multiple_inputs:
-        x2 = embedding_layer(input_2)
-        x2 = tf.keras.layers.Flatten()(x2)
-        x3 = tf.concat([x1, x2], 1)
+        input_1, input_2 = tf.split(inputs, 2)
+        x1 = embedding_layer(input_1)
+        x1 = tf.keras.layers.Flatten()(x1)
     else:
-        x3 = x1
-    # Output Layer
-    predictions = tf.keras.layers.Dense(num_of_classes, activation=None)(x3)
+        x1 = embedding_layer(inputs)
+        x2 = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)) (x1)
+    
+    predictions = tf.keras.layers.Dense(256, activation=None)(x2)
     # Build model
     return tf.keras.models.Model(inputs=inputs, outputs=predictions)
 
-def get_model_LSTM():
+def get_model_LSTM(vocab_size):
     max_features = 20000  # Only consider the top 20k words
     return tf.keras.Sequential(
     [
         tf.keras.Input(shape=(None,), dtype="int32"),
-        tf.keras.layers.Embedding(max_features, 128),
+        tf.keras.layers.Embedding(vocab_size, 128),
         tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, return_sequences=True)),
         tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
         tf.keras.layers.Dense(256, activation=None)
     ])
 
-def get_model_FF():
-    max_features = 20000  # Only consider the top 20k words
+def get_model_FF(vocab_size, embedding_dim):
     return tf.keras.Sequential([
 #        tf.keras.Input(input_shape=(1,), dtype="int32"),
-#        tf.keras.layers.Embedding(max_features, 128, input_length=2),
+        tf.keras.layers.Embedding(vocab_size, embedding_dim),
 #        tf.keras.layers.Flatten(),
-#        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, return_sequences=True)),
+        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, return_sequences=True)),
 #        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
         tf.keras.layers.Dense(256, activation=None)
 #        tf.keras.layers.Dense(256, input_dim=2, activation=None)
@@ -141,19 +135,13 @@ y_train.shape: (10,): bool
 '''
 def get_data_numerical():
     maxlen = 20000
-    x_train = np.random.rand(maxlen, 1)
-    for item in x_train:
-        item[0] = round(item[0], 3)
-    y_train = np.zeros(x_train.shape)
-    for id_r, item_r in enumerate(x_train):
-        y_train[id_r][0] = decide(item_r[0])
+    x_train = np.random.randint(10, size=(maxlen, 4))
     
-    x_val = np.random.rand(maxlen, 1)
-    for item in x_val:
-        item[0] = round(item[0], 3)
-    y_val = np.zeros(x_val.shape)
-    for id_r, item_r in enumerate(x_val):
-        y_val[id_r][0] = decide(item_r[0])
+    y_train = np.random.randint(2, size=(maxlen))
+    
+    x_val = np.random.randint(10, size=(maxlen, 4))
+    
+    y_val = np.random.randint(2, size=(maxlen))
                 
     return (x_train, y_train), (x_val, y_val)
     
@@ -238,6 +226,15 @@ def convert_int_to_array(int_val):
         arr = [0] + arr
     return np.asarray(arr)
 
+def convert_to_vocab_zero(data_arr, mul_factor=10000, int_to_char=False):
+    dest = np.empty_like(data_arr, dtype=list)
+    for id_r, item_r in enumerate(data_arr):
+        if int_to_char:
+            dest[id_r] = convert_int_to_array(int(item_r * mul_factor))
+        else:
+            dest[id_r] = int(item_r * mul_factor)
+    return dest
+
 def convert_to_vocab(data_arr, mul_factor=10000, int_to_char=False):
     dest = np.empty_like(data_arr, dtype=list)
     for id_r, item_r in enumerate(data_arr):
@@ -258,7 +255,7 @@ def concate_rows(data_arr):
 def main():
     
     _type = sys.argv[1]
-    input_size = 2
+    input_size = 1
     if _type == 'mnist':
         model = get_model()
         train_dataset, test_dataset = get_data()
@@ -267,12 +264,15 @@ def main():
         (x_train, y_train), (x_val, y_val) = get_data_text()
         test_dataset = (x_val, y_val)
     elif _type == 'func':
-        model = get_model_LSTM_char(10, 4, add_multiple_inputs=True)
-        (x_train, y_train), (x_val, y_val) = get_data_numerical_meta(input_size, decide_var)
+#        model = get_model_LSTM(10)
+        model = get_model_LSTM_char(10, 4, add_multiple_inputs=False)
+#        model = get_model_FF(10, 128)
+        (x_train, y_train), (x_val, y_val) = get_data_numerical()
+#        (x_train, y_train), (x_val, y_val) = get_data_numerical_meta(input_size, decide_var)
 #        x_train = concate_rows(convert_to_vocab(x_train, int_to_char=True))
 #        x_val = concate_rows(convert_to_vocab(x_val, int_to_char=True))
-        x_train = convert_to_vocab(x_train, int_to_char=True)
-        x_val = convert_to_vocab(x_val, int_to_char=True)
+#        x_train = convert_to_vocab_zero(x_train, int_to_char=True)
+#        x_val = convert_to_vocab_zero(x_val, int_to_char=True)
 #        x_train_unique = set(x_train.flatten())
 #        x_val_unique = set(x_val.flatten())
         test_dataset = (x_val, y_val)
