3c2c2f52a15ac19e23a38b51143c42a4c671fc54
mpatsis <mpatsis13@gmail.com>
2021-01-31 23:29:59 +0000

    First implementation for a model that learns constraint embeddings.

---
+++ learn_constrain_embeddings.py
@@ -0,0 +1,168 @@
+import io
+import numpy as np
+import pdb
+import sys
+
+import tensorflow as tf
+import tensorflow_addons as tfa
+import tensorflow_datasets as tfds
+
+
+def get_char_LSTM(batch_size, vocab_size=11, embedding_size=512, variables=1, bidirectional=True, share_embeddings=True):
+    lstm_features = 512
+    if share_embeddings:
+        embedding_layer = tf.keras.layers.Embedding(
+            vocab_size, embedding_size, embeddings_initializer='uniform',
+            input_length=None
+        )
+        inputs = tf.keras.layers.Input(shape=(variables, None, ), name='input', dtype='int64')
+        embeddings = embedding_layer(inputs)
+        print(embeddings)
+        embeddings = tf.unstack(embeddings, variables, axis=1)
+        print(embeddings)
+        
+        lstm_outs = []
+        for i in range(variables):
+            if bidirectional:
+                lstm_outs.append(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(lstm_features)) (embeddings[i]))
+            else:
+                lstm_outs.append(tf.keras.layers.LSTM(lstm_features) (embeddings[i]))
+        print(lstm_outs)
+        lstm_out = tf.stack(lstm_outs, 1)
+        print('variables', variables)
+        lstm_out = tf.reshape(lstm_out, [-1, variables * lstm_features])
+        predictions = tf.keras.layers.Dense(2, activation='softmax', dtype='float32')(lstm_out)
+
+        # if bidirectional:
+        #     lstm_outs = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(lstm_features)) (embeddings)
+        # else:
+        #     lstm_outs = tf.keras.layers.LSTM(lstm_features) (embeddings)
+        # layer1 = tf.keras.layers.Dense(128, activation='relu')(lstm_outs)
+        # predictions = tf.keras.layers.Dense(2, activation='softmax', dtype='float32')(lstm_outs)
+    
+    # # Build model
+    return tf.keras.models.Model(inputs=inputs, outputs=predictions), lstm_out
+
+
+def loss(model, x, y, training, loss_object):
+    # training=training is needed only if there are layers with different
+    # behavior during training versus inference (e.g. Dropout).
+    y_ = model(x, training=training)
+    # print('x:', x, x.shape)
+    # print('y:', y_)
+    return loss_object(y_true=y, y_pred=y_)
+
+
+def grad(model, inputs, targets, loss_object):
+    with tf.GradientTape() as tape:
+        loss_value = loss(model, inputs, targets, True, loss_object)
+    return loss_value, tape.gradient(loss_value, model.trainable_variables)
+
+
+def model_fit(model, x_train, y_train, loss_object, optimizer, epochs=1):
+    # pdb.set_trace()
+    # Create batches
+    for e in range(epochs):
+        for i in range(0, len(x_train), 32):
+            batch_x = x_train[i : i + 32]
+            # print(batch_x)
+            batch_y = y_train[i : i + 32]
+            loss_value, grads = grad(model, batch_x, batch_y, loss_object)
+            optimizer.apply_gradients(zip(grads, model.trainable_variables))
+    # for idx, x in enumerate(x_train):
+    #     print(x)
+    #     y = y_train[idx]
+    #     # Optimize the model
+    #     loss_value, grads = grad(model, x, y, loss_object)
+    #     print(loss_value)
+    #     optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+def accuracy(predictions, values):
+    correct = 0.0
+    for prediction, value in zip(predictions, values):
+        if prediction - value == 0.0:
+            correct += 1
+    
+    return correct / len(predictions)
+
+
+def main():
+    maxlen = 32 * 500
+    variables = 2
+    _type = sys.argv[1]
+    # input_size = 2
+    if _type == 'func':
+#        model = get_model_LSTM(10)
+        num_var = 1
+        model, lstm_out = get_char_LSTM(32, bidirectional=False, variables=variables)
+        print('lstm_out:', lstm_out)
+        x_train = np.random.randint(11, size=(maxlen, variables, 4))
+        y_train = [1 if np.sum(x) < 40 else 0 for x in x_train]
+        print(x_train[: 32], [np.sum(x) for x in x_train[: 32]])
+        print(y_train[: 50])
+        # y_train = np.random.randint(2, size=(maxlen))
+        # print(x_train)
+        p = model.predict(x_train)
+        print(p[: 32])
+
+        x_test = np.random.randint(11, size=(32, variables, 4))
+        y_test = [1 if np.sum(x) < 40 else 0 for x in x_test]
+        # y_test = np.random.randint(2, size=(32))
+        print('y_test', y_test)
+
+        prediction_probs = model.predict(x_test)
+        predictions = [int(np.round(p[1])) for p in prediction_probs]
+        print(prediction_probs)
+        print(predictions)
+        acc = accuracy(predictions, y_test)
+        print('Accuracy:', acc)
+    # elif _type == 'func_star':
+    #     model = get_model_FF()
+    #     (x_train, y_train), (x_val, y_val) = get_data_numerical_meta(input_size, decide_var)
+    #     x_train = convert_to_vocab(x_train)
+    #     x_val = convert_to_vocab(x_val)
+    #     test_dataset = (x_val, y_val)
+
+#    loss_obj = tfa.losses.TripletSemiHardLoss()
+    loss_obj = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    optimizer_obj = tf.keras.optimizers.Adam(0.001)
+    # Compile the model
+#    print(model.summary())
+#    model.compile(
+#        optimizer=tf.keras.optimizers.Adam(0.001),
+#        loss=tfa.losses.TripletSemiHardLoss())
+#    model.compile(
+#        optimizer=tf.keras.optimizers.Adam(0.001),
+#        loss=tf.keras.losses.BinaryCrossentropy())
+    if _type == 'func' or _type == 'func_star':
+        for e in range(3):
+            model_fit(model, x_train, y_train, loss_obj, optimizer_obj)
+    #        history = model.fit(x_train, y_train, batch_size=32, epochs=8)
+            prediction_probs = model.predict(x_test)
+            predictions = [int(np.round(p[1])) for p in prediction_probs]
+            print(prediction_probs)
+            print(predictions)
+            acc = accuracy(predictions, y_test)
+            print('Accuracy:', acc)
+        
+    # Save test embeddings for visualization in projector
+    # test_loss = model.evaluate(x_test, y_test)
+    # print('test loss: ' + str(test_loss))
+    # np.savetxt("vecs.tsv", results, delimiter='\t')
+
+#    out_m = io.open('meta.tsv', 'w', encoding='utf-8')
+#    for img, labels in tfds.as_numpy(test_dataset):
+#        [out_m.write(str(x) + "\n") for x in labels]
+#    out_m.close()
+
+
+    # try:
+    #     from google.colab import files
+    #     files.download('vecs.tsv')
+    #     files.download('meta.tsv')
+    # except:
+    #     pass
+
+
+if __name__ == "__main__":
+    main()
